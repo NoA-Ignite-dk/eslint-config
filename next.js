ff45d0db263a35d9a1612cc7ab7c563862f08756
mthines <mads.thines@noaignite.com>
2022-03-02 09:02:27 +0100

    rule(next): added simple sorting

--- next.js
+++ next.js
@@ -6,4 +6,38 @@ module.exports = {
 		'./react.js',
 		'plugin:@next/next/core-web-vitals',
 	],
+	plugins: ['simple-import-sort', 'unused-imports'],
+	rules: {
+		'simple-import-sort/imports': [
+			'warn',
+			{
+				groups: [
+					['^((?!((.*).(scss|css))|assets|src|./|../|@[^storybook|^sentry]))'],
+					['^(@[^storybook|^sentry](.*))'],
+					['^(@apps/|apps/|@libs/)'],
+					['^(components/)'],
+					['^(config/)'],
+					['^(lib/)'],
+					['^(pages/)'],
+					['^(state/)'],
+					['^(styles/)'],
+					['^(translations/)'],
+					['^(types/)'],
+					['^(utils/)'],
+					['^(assets/|(.*).(scss|css)$)'],
+					['^((./|../))'],
+				],
+			},
+		],
+		'unused-imports/no-unused-imports': 'error',
+		'unused-imports/no-unused-vars': [
+			'warn',
+			{
+				args: 'after-used',
+				argsIgnorePattern: '^_',
+				vars: 'all',
+				varsIgnorePattern: '^_',
+			},
+		],
+	},
 };
