701514c3310496b20ce50094f50734c4ec67fb93
mthines <mads.thines@noaignite.com>
2022-03-29 12:19:09 +0200

    rule(next): added padding-line-between-statements

--- next.js
+++ next.js
@@ -9,6 +9,44 @@ module.exports = {
 	plugins: ['unused-imports'],
 	rules: {
 		'import/no-extraneous-dependencies': ['off'],
+		'padding-line-between-statements': [
+			'warn',
+			{
+				blankLine: 'always',
+				next: '*',
+				prev: ['block', 'block-like', 'cjs-export', 'class', 'export', 'function', 'break'],
+			},
+			{
+				blankLine: 'always',
+				next: ['return'],
+				prev: ['multiline-const', 'function', 'singleline-const'],
+			},
+			{
+				blankLine: 'always',
+				next: ['singleline-const'],
+				prev: ['multiline-const', 'function'],
+			},
+			{
+				blankLine: 'always',
+				next: ['multiline-const', 'function'],
+				prev: ['singleline-const'],
+			},
+			{
+				blankLine: 'never',
+				next: ['singleline-const'],
+				prev: ['singleline-const'],
+			},
+			{
+				blankLine: 'any',
+				next: ['export', 'import'],
+				prev: ['export', 'import'],
+			},
+			{
+				blankLine: 'always',
+				next: ['if'],
+				prev: ['*'],
+			},
+		],
 		'unused-imports/no-unused-imports': ['error'],
 		'unused-imports/no-unused-vars': ['warn',
 			{
