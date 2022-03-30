743e2a6ddb0149173fa3cb51497304d217e9e7b0
mthines <mads.thines@noaignite.com>
2022-03-30 16:25:16 +0200

    fix(ts): moved unused to typescript from next

--- next.js
+++ next.js
@@ -6,17 +6,4 @@ module.exports = {
 		'./react.js',
 		'plugin:@next/next/core-web-vitals',
 	],
-	plugins: ['unused-imports'],
-	rules: {
-		'import/no-extraneous-dependencies': ['off'],
-		'unused-imports/no-unused-imports': ['error'],
-		'unused-imports/no-unused-vars': ['warn',
-			{
-				args: 'after-used',
-				argsIgnorePattern: '^_',
-				vars: 'all',
-				varsIgnorePattern: '^_',
-			},
-		],
-	},
 };
